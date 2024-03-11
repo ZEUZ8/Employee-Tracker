5b8f079785e71963a6c613c60fbb34752e860f22
sinan <ptsinan8590@gmail.com>
2024-03-11 09:58:04 +0530

    added the pb in the footer for better view

--- src/Components/HomePage/CenterSection.jsx
+++ src/Components/HomePage/CenterSection.jsx
@@ -45,7 +45,7 @@ const CenterSection = () => {
         </div>
       </div>
 
-      <div className="extra flex justify-center items-center w-full px-2">
+      <div className="extra flex justify-center items-center w-full px-2 pb-4">
         <div className="flex-1">
           <div className=" bg-white h-[3.5rem] flex  items-center justify-between rounded-2xl p-3">
             <div className="text-lg font-bold text-gray-400">
