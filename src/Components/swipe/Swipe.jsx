bb0805813641fc14a8889064c77e7dedc8908ebf
sinan <ptsinan8590@gmail.com>
2024-03-13 21:25:49 +0530

    Implemented time tracking functionality to achieve check-in and check-out status along with timestamps, storing them in a state. Next step involves persisting the state in the database to track user activity, completing our goal of comprehensive user tracking.

--- src/Components/swipe/Swipe.jsx
+++ src/Components/swipe/Swipe.jsx
@@ -1,25 +1,46 @@
-import React, { useRef, useState } from 'react';
-import "./sinan.css"
+import React, { useEffect, useRef, useState } from "react";
+import "./sinan.css";
 import { FaArrowRight } from "react-icons/fa6";
 
 const Swipe = () => {
-  const color = 'white';
+  const color = "white";
   const [unlocked, setUnlocked] = useState(false);
+  const [status, setStatus] = useState("CHECK IN");
+  const [time, setTime] = useState({ hour: "", minute: "", status: "" });
+  const [history, setHistory] = useState([]);
   const slider = useRef(null);
   const container = useRef(null);
   const initialPosition = useRef(null);
+  const timeoutRef = useRef(null);
+
+  useEffect(() => {
+    if (unlocked) {
+      setStatus((prev) => {
+        if (prev === "CHECK OUT") {
+          return "CHECK IN";
+        } else {
+          return "CHECK OUT";
+        }
+      });
+      timeoutRef.current = setTimeout(() => {
+        setUnlocked(false);
+        slider.current.style.left = "50px"; // Reset slider position
+      }, 3000);
+    }
+    return () => clearTimeout(timeoutRef.current); // Clear timeout on component unmount or state change
+  }, [unlocked]);
 
   const startDrag = (event) => {
-    event.preventDefault();
+    // event.preventDefault();
     initialPosition.current = event.clientX || event.touches[0].clientX;
-    document.addEventListener('mousemove', handleDrag);
-    document.addEventListener('touchmove', handleDrag);
-    document.addEventListener('mouseup', stopDrag);
-    document.addEventListener('touchend', stopDrag);
+    document.addEventListener("mousemove", handleDrag);
+    document.addEventListener("touchmove", handleDrag);
+    document.addEventListener("mouseup", stopDrag);
+    document.addEventListener("touchend", stopDrag);
   };
 
   const handleDrag = (event) => {
-    event.preventDefault();
+    // event.preventDefault();
     const currentPosition = event.clientX || event.touches[0].clientX;
     const difference = currentPosition - initialPosition.current;
     const containerWidth = container.current.offsetWidth;
@@ -46,27 +67,54 @@ const Swipe = () => {
   };
 
   const getText = () => {
-    return unlocked ? 'Marked' : 'Slide to Unlock';
+    return unlocked ? "Check Out" : "CHECK IN";
   };
 
+  useEffect(() => {
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    setTime({ time: hours, minute: minutes, status: status });
+
+    if (time.status) {
+      setHistory((prev) => {
+        return [...prev, time];
+      });
+    }
+  }, [status]);
+
+  useEffect(() => {
+    console.log(history,' CONSOLING THE USER HISTORY');
+  }, [history]);
+
   return (
-      <div className='ReactSwipeButton   flex justify-center items-center'>
-        <div className={`rsbContainer  ${unlocked ? 'rsbContainerUnlocked' : ''}`} ref={container}>
-          <div
-            className='rsbcSlider '
-            ref={slider}
-            onMouseDown={startDrag}
-            onTouchStart={startDrag}
-            style={{ background: color,paddingLeft:'2rem' }}
-          >
-            <span className='rsbcSliderText  '>{getText()}</span>
-            <span className='rsbcSliderArrow '><FaArrowRight/></span>
-            <span className='rsbcSliderCircle ' style={{ background: color}}></span>
-          </div>
-          <div className='rsbcText'>{getText()}</div>
+    <div className="ReactSwipeButton   flex justify-center items-center">
+      <div
+        className={`rsbContainer  ${unlocked ? "rsbContainerUnlocked" : ""}`}
+        ref={container}
+      >
+        <div
+          className={`rsbcSlider`}
+          ref={slider}
+          onMouseDown={startDrag}
+          onTouchStart={startDrag}
+          style={{ background: color }}
+        >
+          <span className={`rsbcSliderText ${"text-green-600"}`}>Marked</span>
+          <span className="rsbcSliderArrow ">
+            <FaArrowRight />
+          </span>
+          <span className="rsbcSliderCircle "></span>
+        </div>
+        <div
+          className={`rsbcText ${
+            status === "CHECK OUT" ? "text-red-700" : "text-white"
+          }`}
+        >
+          {status}
         </div>
       </div>
-
+    </div>
   );
 };
 
